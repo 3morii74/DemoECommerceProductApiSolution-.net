f16f7f90b05e1f66649c14d904d389da73bb01db
3morii74 <omerahmed200237@gmail.com>
2025-01-20 20:59:33 +0200

    crud for ProductRepository

---
+++ ProductApi.Application/Inerfaces/IProduct.cs
@@ -0,0 +1,14 @@
+﻿using eCommmerce.SharedLibrary.Interface;
+using productApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApi.Application.Inerfaces
+{
+    public interface IProduct : IGenericInterface<Product>
+    {
+    }
+}
---
+++ ProductApi.Infrastructure/Data/ProductDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using productApi.Domain.Entities;
+
+namespace ProductApi.Infrastructure.Data
+{
+    public class ProductDbContext(DbContextOptions<ProductDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
